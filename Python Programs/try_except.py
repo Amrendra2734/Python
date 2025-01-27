c556fad510bdb789fd6e70911780eb013e5ce67a
Amrendra <amrendraranchi.27@gmail.com>
2025-01-28 01:07:51 +0530

    Python Basics

--- Python Programs/try_except.py
+++ Python Programs/try_except.py
@@ -1,6 +1,6 @@
 n= input("Enter a number: ")
 try:
-    N=int(n)
+    N=int(n)                             #try runs this code and if it has any tracebacks then 
 except:
-    print("Not a number")
+    print("Not a number")                #instead of giving a traceback it runs this
 print("Good JOb")
