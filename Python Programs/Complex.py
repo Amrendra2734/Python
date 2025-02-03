05073fd6b284b1f903047fbb49c92d65e019da62
Amrendra <amrendraranchi.27@gmail.com>
2025-02-03 23:25:37 +0530

    Python Basics

---
+++ Python Programs/Complex.py
@@ -0,0 +1,4 @@
+real = int(input("Enter the real part: "))
+imaginary = int(input("Enter the imaginary part: "))
+complex = complex(real,imaginary)                  #built in function to make complex representation
+print(complex)
