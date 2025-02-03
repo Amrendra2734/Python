d3fbf785c9f571b502d606a535ca8755a96096e7
Amrendra <amrendraranchi.27@gmail.com>
2025-02-03 23:25:17 +0530

    Python Basics

---
+++ String Formatting/String_formatting.py
@@ -0,0 +1,10 @@
+name = "Amrendra"
+age = 20
+print(f"My name is {name} and I am {age} years old")
+#or
+print("My name is {} and I am {} years old.".format(name,age))
+#you can also format numbers
+say = 109.9230923023
+print(f"{say:.4f}") #This will round the number upto 4 decimal
+num= 100000000000
+print(f"{num:,}")  #This will print the numbers with commas in thousands
--- String/String_slicing.py
+++ String/String_slicing.py
@@ -1,6 +1,9 @@
 str='Github repository'
+print(len(str))
 print(str[0:6])                #upto 6 but does not include 6
 print(str[:3])                 #starts from 0
 print(str[7:11])            
 print(str[7:])                #goes till last
 print(str[:])                 #basically print(str)
+print(str[1:10:2])            #prints with 2 skips
+print(str[::-1])              #reverses the string , basically the steps are inversed
